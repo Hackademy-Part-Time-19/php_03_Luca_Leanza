5db158b09bd486d06d512ce63a232c754c256506
Lleanza7 <lucaleanza92@hotmail.com>
2024-01-16 20:01:37 +0100

    esercizio completato

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+class Company {
+    public $name;
+    public $location;
+    public $totEmployees = 0;
+
+    public static $TotalCompanies = 0;
+    public static $TotalEmployeesOfAllCompanies = 0;
+    public static $totalAvgAnnualWage = 0;
+    public static $avgWage = 1500;
+
+    public function __construct($_name, $_location, $_tot_employees)
+    {
+        $this->name = $_name;
+        $this->location = $_location;
+        $this->totEmployees = $_tot_employees;
+
+        self::$TotalEmployeesOfAllCompanies += $this->totEmployees;
+        self::$TotalCompanies++;
+        
+    }
+    
+    public function PrintDescription() {
+        if ($this->totEmployees > 1) {
+            echo "The company $this->name is located in $this->location and has $this->totEmployees employees.\n";
+        } else {
+            echo "The company $this->name is located in $this->location and does not have employees.\n";
+        }
+    }
+
+    public function AvgAnnualWage(){
+        $avg_annual_wage = (self::$avgWage * $this->totEmployees) * 12 ;
+        echo "$this->name's average annual spend is $avg_annual_wage\n";
+
+        self::$totalAvgAnnualWage += $avg_annual_wage;
+        
+    }
+    
+    public static function PrintTotal () {
+        echo "The total of all companies is " .  self::$TotalCompanies . "\n";
+        echo "The total of all employees is " .  self::$TotalEmployeesOfAllCompanies . "\n";
+        echo "The total of all Avg Annual Wage is " .  self::$totalAvgAnnualWage . "\n";
+    }
+}
+
+$company1 = new Company('Apple', 'Cupertino', 10000);
+$company1->PrintDescription();
+$company1->AvgAnnualWage();
+$company2 = new Company('Google', 'California', 20000);
+$company2->PrintDescription();
+$company2->AvgAnnualWage();
+$company3 = new Company('Facebook', 'USA', 30000);
+$company3->PrintDescription();
+$company3->AvgAnnualWage();
+$company4 = new Company('Amazon', 'Seattle', 40000);
+$company4->PrintDescription();
+$company4->AvgAnnualWage();
+$company5 = new Company('Microsoft', 'Redmond', 50000);
+$company5->PrintDescription();
+$company5->AvgAnnualWage();
+
+Company::PrintTotal();
+
+
